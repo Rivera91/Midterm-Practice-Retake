d4852da4153f307a89311b21327eba0e54df5407
Amy Rivera <amyrivera.ar@gmail.com>
2018-01-16 17:43:31 -0500

    added part01

--- app/src/main/java/nyc/c4q/midtermretake/LoginActivity.java
+++ app/src/main/java/nyc/c4q/midtermretake/LoginActivity.java
@@ -3,13 +3,11 @@ package nyc.c4q.midtermretake;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoopActivity extends AppCompatActivity {
-
-
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_loop);
+        setContentView(R.layout.activity_login);
     }
 }
